254862a14cd73baed9cb733330cb65abd3a83993
JuYanYan <liu.baorui@yahoo.com>
2021-08-13 21:27:21 +0800

    添加了例子程序

--- pbdemo.cpp
+++ pbdemo.cpp
@@ -18,15 +18,56 @@
 #include "pb.hpp"
 #include <format>
 #include <iostream>
-
-int main()
+#include <assert.h>
+//
+int main(int argc, char **argv)
 {
     using namespace Pb;
+    //
+    if (argc != 2)
+    {
+        std::cout << "pbdemo [Args...]"       << std::endl
+                  << "Args:"                  << std::endl
+                  << "    a decimal integer." << std::endl;
+        return EXIT_FAILURE;
+    }
     // parser
-    
-    // input
-    ConstString str = "1.234";
-
-
-    return 0;
+    // clang-format off
+    auto numbers = InRange<ConstString>('0', '9') * N(1, Infinity)
+                >> [](const std::vector<unichar> &list) -> Result<uint64_t>
+                {
+                    uint64_t res = 0;
+                    for (const auto &i : list)
+                    {
+                        const uint64_t val = static_cast<uint64_t>(i - '0');
+                        const uint64_t tmp = res;
+                        res = res * 10 + val;
+                        // 检查运算溢出
+                        if (res < tmp)
+                        {
+                            return Fatal<uint64_t>("The integer constant is too large.");
+                        }
+                    }
+                    return Success<uint64_t>(res);
+                };
+    // clang-format on
+    // 输入
+    ConstString str = argv[1];
+    // 执行分析
+    std::cout << std::format(">> {}", str.c_str()) << std::endl;
+    bool succ = numbers.Parse(str, [](const Result<uint64_t> &res, const ConstString &nextstr)
+    {
+        if (res.label == Label::Success)
+        {
+            std::cout << std::format(">> Success.\n   result: {}\n   then: '{}'", res.succ_val, nextstr.c_str()) << std::endl;
+            return true;
+        }
+        else {
+            std::cout << std::format(">> Error.\n   {}", res.failed_val.msg) << std::endl;
+            return false;
+        }
+    });
+    std::cout << std::format(">> {}", succ) << std::endl;
+    //
+    return EXIT_SUCCESS;
 }
